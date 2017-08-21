c6b1a794a50b967ee3f059c4ae70068b420b5c15
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-08-21 14:59:18 +0200

    added photo columns to the tables users and drones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821122829) do
+ActiveRecord::Schema.define(version: 20170821125739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170821122829) do
     t.string   "lng"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.index ["user_id"], name: "index_drones_on_user_id", using: :btree
   end
 
@@ -83,6 +84,7 @@ ActiveRecord::Schema.define(version: 20170821122829) do
     t.string   "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "photo"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
