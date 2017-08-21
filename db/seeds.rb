915b4e1ad7a2bdd9dd5013ee870349cfcdf270b8
Gerardo Bonilla <gerbonilla@gmail.com>
2017-08-21 15:07:19 +0200

    seed file creates users and drones - no bookings yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#create 3 users
+peter = User.create(email: "peter@wagon.com", first_name: "Peter", last_name: "Griffin", password:"123456", password_confirmation: "123456", description: "Avid drone enthusiast")
+stewie = User.create(email: "stewie@wagon.com", first_name: "Stewie", last_name: "Griffin", password:"123456", password_confirmation: "123456", description: "Drone visionaire")
+meg = User.create(email: "meg@wagon.com", first_name: "Meg", last_name: "Griffin", password:"123456", password_confirmation: "123456", description: "Beginner drone flier")
+
+sample_users = [peter, stewie, meg]
+
+# create 3 drones per user
+
+sample_users.each do |user|
+  3.times do
+    new_drone = Drone.create(user: user, title:Faker::Superhero.name, description: Faker::Superhero.power, rate: rand(10..200), address: Faker::Address.street_address)
+    new_feature = Feature.create(drone: new_drone, category: "Professional", max_alt: 400, battery_life: "10 hours")
+  end
+end
+
+# #create bookings
+# t.integer  "user_id"
+# t.integer  "drone_id"
+# t.string   "status"
+# t.date     "start_date"
+# t.date     "end_date"
+# t.integer  "original_rate"
+
+
+
+
