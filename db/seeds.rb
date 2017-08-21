07fc6025bfeb8dc906226da618fed9f8680167c9
Johannes Vaisfeld <jvaisfeld@gmail.com>
2017-08-21 15:51:15 +0200

    added seed variables plus changed navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+drone_categories = ["close range", "short range", "mid range", "endurance"]
+drone_max_alt = [50, 80, 100, 150, 200, 300, 500, 1000]
+drone_range = ["100m", "150m", "200m", "300m", "500m", "1000m", "3000m", "7000m"]
+battery_life = ["10 min", "15 min", "20 min", "25 min", "30 min", "45 min", "60 min"]
+
+
+
