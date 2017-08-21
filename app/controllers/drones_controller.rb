05d0ef41317421036b994642910dbfbaf06e9a10
John Fish <j31@users.noreply.github.com>
2017-08-21 15:48:32 +0200

    drones controller and routes resources

--- app/controllers/drones_controller.rb
+++ app/controllers/drones_controller.rb
@@ -1,4 +1,6 @@
 class DronesController < ApplicationController
+  before_action :set_drone, only: [:show, :edit, :update, :destroy]
+
   def index
     @drones = Drone.all
   end
@@ -10,14 +12,35 @@ class DronesController < ApplicationController
   end
 
   def create
+     @drone = Drone.new(drone_params)
+    if @drone.save
+      redirect_to drone_path(@drone)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @drone = Drone.new(drone_params)
+    if @drone.update
+      redirect_to drone_path(@drone)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @drone.destroy
+    redirect_to drones_path
   end
+
+  private
+
+  def set_cocktail
+    @drone = Drone.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
 
-  get 'bookings/show'
-
-  get 'bookings/new'
-
-  get 'bookings/create'
-
-  get 'drones/index'
-
-  get 'drones/show'
-
-  get 'drones/new'
-
-  get 'drones/create'
-
-  get 'drones/edit'
-
-  get 'drones/update'
-
-  get 'drones/destroy'
+  resources :drones do
+    resources :bookings, only: [ :create ]
+  end
 
   devise_for :users
   root to: 'pages#home'
