dc03e62bd10f6e83b8b83f517eaeedee3e95ae39
Gerardo Bonilla <gerbonilla@gmail.com>
2017-08-22 10:58:06 +0200

    form for new drone includes dropdown

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -3,4 +3,9 @@ class Drone < ApplicationRecord
   has_many :bookings
 
   has_attachment :photo
+
+DRONE_CATEGORIES = ["close range", "short range", "mid range", "endurance"]
+DRONE_MAX_ALT = [50, 80, 100, 150, 200, 300, 500, 1000]
+DRONE_RANGE = ["100m", "150m", "200m", "300m", "500m", "1000m", "3000m", "7000m"]
+DRONE_BATTERY_LIFE = ["10 min", "15 min", "20 min", "25 min", "30 min", "45 min", "60 min"]
 end
