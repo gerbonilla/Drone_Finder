9d1f735b77c1e9c3466453948d99298ca5e9cc76
John Fish <j31@users.noreply.github.com>
2017-08-23 13:25:20 +0200

    add map box framework

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 20170823092640) do
     t.string   "description"
     t.integer  "rate"
     t.string   "address"
-    t.float    "lat"
-    t.float    "lng"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "category"
