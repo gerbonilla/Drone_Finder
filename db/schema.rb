701d9fe5a890af35f3031dd45f4d559374a2c9d7
Gerardo Bonilla <gerbonilla@gmail.com>
2017-08-23 16:48:38 +0200

    some small change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822102025) do
+ActiveRecord::Schema.define(version: 20170823092640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,8 @@ ActiveRecord::Schema.define(version: 20170822102025) do
     t.string   "description"
     t.integer  "rate"
     t.string   "address"
-
     t.float    "latitude"
     t.float    "longitude"
-    
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "category"
