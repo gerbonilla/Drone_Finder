c61ecb87ffbf19718068e557c830649f7544c196
Fredrik Nordmark <fredrik.nordmark@telia.com>
2017-08-24 12:37:38 +0200

    added message to booking_params in bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -45,6 +45,6 @@ class BookingsController < ApplicationController
 
 private
   def booking_params
-    params.require(:booking).permit(:user_id, :drone_id, :start_date, :end_date, :original_rate, :status)
+    params.require(:booking).permit(:user_id, :drone_id, :start_date, :end_date, :original_rate, :status, :message)
   end
 end
